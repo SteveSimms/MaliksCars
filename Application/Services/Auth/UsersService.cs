931c1d5762ca0cb925e3a91f64c72dfb5bd06fb5
Steven Simms <simmsstev@gmail.com>
2023-06-09 12:48:05 -0700

    Implemented Role assignment in admin dashboard
    Implemented Role assignment in admin dashboard using select menus.

--- Application/Services/Auth/UsersService.cs
+++ Application/Services/Auth/UsersService.cs
@@ -44,59 +44,107 @@ namespace MaliksCars.Application.Services.Auth
             await context.UserRoles.AddAsync(new UserRole { RoleId = 1, User = user });
             await context.SaveChangesAsync();
             return addedUser.Entity;
-       }
-// Eager loading is a technique that allows you to load related data along with the main entity when querying the database. This is done by using the Include method 
-// in your query to specify the navigation properties that you want to eagerly load. You can also use the ThenInclude method to eagerly load nested navigation properties.
+        }
+        // Eager loading is a technique that allows you to load related data along with the main entity when querying the database. This is done by using the Include method 
+        // in your query to specify the navigation properties that you want to eagerly load. You can also use the ThenInclude method to eagerly load nested navigation properties.
         public async Task<List<User>> GetAllUsersAsync()
         {
             using var context = _factory.CreateDbContext();
-            return await context.Users 
+            return await context.Users
                     .Include(u => u.UserRoles)
                     .ThenInclude(ur => ur.Role)
                     .ToListAsync();
         }
 
         //asign user roles 
-        public async Task AssignAdminRoleAsync(int userId )
-            {
+        public async Task AssignAdminRoleAsync(int userId)
+        {
             using var context = _factory.CreateDbContext();
             var user = await context.Users.FindAsync(userId);
 
-                if(user != null)
+            if (user != null)
+            {
+                // Get the Admin role
+                var adminRole = await context.Roles.FirstOrDefaultAsync(r => r.Name == CustomRoles.Admin);
+
+                // Check if the user is already assigned to the Admin role
+                var userRole = await context.UserRoles.FirstOrDefaultAsync(ur => ur.UserId == userId && ur.RoleId == adminRole.Id);
+
+                // Add the user to the Admin role if they are not already assigned
+                if (adminRole != null && userRole == null)
                 {
-                    // Get the Admin role
-                    var adminRole = await context.Roles.FirstOrDefaultAsync(r => r.Name == CustomRoles.Admin);
+                    await context.UserRoles.AddAsync(new UserRole { RoleId = adminRole.Id, User = user });
+                    await context.SaveChangesAsync();
+                }
+            }
 
-                    // Check if the user is already assigned to the Admin role
-                    var userRole = await context.UserRoles.FirstOrDefaultAsync(ur => ur.UserId == userId && ur.RoleId == adminRole.Id);
 
-                      // Add the user to the Admin role if they are not already assigned
-                    if (adminRole != null && userRole == null)
-                    {
-                        await context.UserRoles.AddAsync(new UserRole { RoleId = adminRole.Id, User = user });
-                        await context.SaveChangesAsync();
-                    }
-             }
-
-           
         }
 
 
-          public async Task<bool> GetUserRolesAsync(int userId)
+
+        public async Task AssignRoleAsync(int userId, string roleName)
+        {
+            using var context = _factory.CreateDbContext();
+            var user = await context.Users.FindAsync(userId);
+
+            if (user != null)
             {
+                // Get the role
+                var role = await context.Roles.FirstOrDefaultAsync(r => r.Name == roleName);
 
-                using var context = _factory.CreateDbContext();
+                // Check if the user is already assigned to the role
+                var userRole = await context.UserRoles.FirstOrDefaultAsync(ur => ur.UserId == userId && ur.RoleId == role.Id);
 
-                var user = await context.Users.Include(u => u.UserRoles)
-                            .ThenInclude(ur => ur.Role)
-                            .FirstOrDefaultAsync(u => u.Id == userId);
-
-                     if(user != null)
-                    {
-                        return user.UserRoles.Any(ur => ur.Role.Name == CustomRoles.Admin);
-                    }
-                return false;
+                // Add the user to the role if they are not already assigned
+                if (role != null && userRole == null)
+                {
+                    await context.UserRoles.AddAsync(new UserRole { RoleId = role.Id, User = user });
+                    await context.SaveChangesAsync();
+                }
             }
+        }
+
+
+        //remove role
+        public async Task RemoveRoleAsync(int userId, string roleName)
+        {
+            using var context = _factory.CreateDbContext();
+            var user = await context.Users
+                .Include(u => u.UserRoles)
+                .ThenInclude(ur => ur.Role)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user != null)
+            {
+                // Find the UserRole object that represents the association between the user and the role
+                var userRole = user.UserRoles.FirstOrDefault(ur => ur.Role.Name == roleName);
+
+                // Remove the UserRole object if it exists
+                if (userRole != null)
+                {
+                    context.UserRoles.Remove(userRole);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+
+        public async Task<bool> GetUserRolesAsync(int userId)
+        {
+
+            using var context = _factory.CreateDbContext();
+
+            var user = await context.Users.Include(u => u.UserRoles)
+                        .ThenInclude(ur => ur.Role)
+                        .FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user != null)
+            {
+                return user.UserRoles.Any(ur => ur.Role.Name == CustomRoles.Admin);
+            }
+            return false;
+        }
 
         public string GetSha256Hash(string input)
         {
--- todos.md
+++ todos.md
@@ -4,9 +4,35 @@
     3.call service from page
     4.make form reusable 
 [] TODO make CarCard component reusable
+[] TODO: add the ability for admins to do crud on cars Create new cars
+[] TODO: add the ability to upload images
+[] TODO: add the ability to create a wish/watchList
+
+[] TODO: add web scraping to collect car images
 [] TODO: work on user account and dashboard pages
-[] TODO: add my self as an admin
+[x] TODO: add my self as an admin
+[] TODO: add the ability to remove user as an admin
+[] TODO: Assign roles via drop down selector?
 [] TODO: use geo location to link to users local dealership
-[] TODO: figure out styling/flowbite integration or other tailwind plugins
-[] TODO: dark/light mode toggle
+
 [x] TODO: addclick to enlarge function to Details page
+
+[] TODO: create mldotnet model of local dealerships via bing maps or google maps api 
+
+
+UI
+[] TODO: Keep styles basic for now add footer and landing pages
+[] TODO: make page responsive
+
+[] TODO: dark/light mode toggle
+
+
+[] TODO: figure out styling/flowbite integration or other tailwind plugins
+
+
+
+//send to notes         @* @user.UserRoles.FirstOrDefault()?.Role.Name *@
+                @* @foreach (var userRole in user.UserRoles)
+                {
+                    <span>@userRole.Role.Name</span>
+                } *@
